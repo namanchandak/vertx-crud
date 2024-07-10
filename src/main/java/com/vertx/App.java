5535128fc724e646bb0754feab5de11d10b1b9e9
Naman Chandak <naman.chandak@hotmail.com>
2024-07-10 18:01:40 +0530

    routes

---
+++ src/main/java/com/vertx/App.java
@@ -0,0 +1,65 @@
+package com.vertx;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.core.http.HttpServerResponse;
+
+public class App {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        HttpServer httpServer = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+
+        router
+                .route("/hello")
+                .handler(routingContext -> {
+                    System.out.println("came to hello:");
+                    HttpServerResponse response = routingContext.response();
+                    response.setChunked(true);
+                    response.write("Hi ");
+                    routingContext
+                            .vertx()
+                            .setTimer(5000, tid -> routingContext.next());
+                });
+
+        router
+                .route("/hello")
+                .handler(routingContext -> {
+                    System.out.println("came to hello 2:");
+                    HttpServerResponse response = routingContext.response();
+                    response.write("Hi 2");
+                    routingContext
+                            .vertx()
+                            .setTimer(5000, tid -> routingContext.next());
+                });
+
+        router
+                .route("/hello")
+                .handler(routingContext -> {
+                    System.out.println("came to hello 3:");
+                    HttpServerResponse response = routingContext.response();
+                    response.write("Hi 3");
+                    response.end();
+                });
+
+        httpServer
+                .requestHandler(router)
+                .listen(8080, result -> {
+                    if (result.succeeded()) {
+                        System.out.println("Server is now listening on port 8080");
+                    } else {
+                        System.out.println("Failed to bind on port 8080");
+                    }
+                });
+    }
+
+    private static void handleRequest(RoutingContext routingContext) {
+        HttpServerResponse response = routingContext.response();
+        response.putHeader("Content-Type", "text/plain");
+        response.end("Hi, I am Naman");
+    }
+}
